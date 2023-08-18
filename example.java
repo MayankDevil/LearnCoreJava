bff5b4064f89233ad1b2ffd9024bfafa2b19ebf5
Mayank <126972040+MayankDevil@users.noreply.github.com>
2023-08-18 15:55:01 +0000

    18-08-2023

---
+++ example.java
@@ -0,0 +1,14 @@
+class Demo
+{
+	public static void main(String args[])
+	{
+		System.out.print(" execute 1 ");
+	}
+}
+class demo
+{
+	public static void main(String args[])
+	{
+		System.out.println(" execute 2 ");
+	}
+}
---
+++ hello.java
@@ -0,0 +1,24 @@
+/*
+-   Testing pourpose
+*/
+class Test
+{
+	public static void main(String args[])
+	{
+		System.out.print("\n Test Execute \n");
+
+		// Theme theme = new Theme();
+
+		// theme.welcome();
+		// theme.mayank();
+
+		Test self = new Test();
+
+		System.out.print(self.plus(1,2));
+	}
+	int plus(int n1, int n2)
+	{
+		return n1 + n2;
+	}
+}
+// todo logic code appplication here
