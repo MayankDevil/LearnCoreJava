f427919b23be32b0d66fc752626a4652f608c49a
Mayank <126972040+MayankDevil@users.noreply.github.com>
2023-08-20 14:42:03 +0000

    Update

--- class_and_object.java
+++ class_and_object.java
@@ -2,18 +2,40 @@
 	class is blue print of any object that have locate memory by create object.
 	class is also is collection of member variables or functions,
 	In java class is Abstract Datatpe.
-
+	object is real entity the is represent class in real world.
 */
 class A
 {
+	int n; // instance variable : that is comman for  member function and it default value is set by javac
+	void f()
+	{
+		n = 10;
+	}
 }
 class B
 {
+	void f()
+	{
+		int n; // local variable : that declare in any function have not any default value.
+		n = 12; // that not print without assign any value.
+	}
 }
 class C
 {
 	public static void main(String args[])
 	{
-		System.out.println(" execute ");
+		// ClassName refference_variable = new Constructor();
+
+		A a = new A();
+		B b = new B();
+
+		a.f();
+		b.f();
+
+		System.out.println(" a.n "+a.n);
 	}
 }
+/*
+ a.n 10 b.n 12
+
+*/
--- example.java
+++ example.java
@@ -1,14 +1,30 @@
-class Demo
+/*
+	Operator : pertor function
+*/
+class Test
 {
+	void arithmetic_operator()
+	{
+		System.out.println(
+			" addition "+(1 + 2)+
+			" subtraction "+(2 - 1)+
+			" Multiplication "+(2 * 3)+
+			" Divition "+(9 / 3)+
+			" Modulas "+(7 % 3)
+		);
+	}
+	void assignment_operator()
+	{
+		System.out.println();
+	}
 	public static void main(String args[])
 	{
-		System.out.print(" execute 1 ");
+		Test t = new Test();
+
+		t.arithmetic_operator();
 	}
 }
-class demo
-{
-	public static void main(String args[])
-	{
-		System.out.println(" execute 2 ");
-	}
-}
+/*
+ addition 3 subtraction 1 Multiplication 6 Divition 3 Modulas 1
+
+*/
