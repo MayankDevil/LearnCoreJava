e5a83147895d7e5631d088d45c68c08bc4591b3c
Mayank <126972040+MayankDevil@users.noreply.github.com>
2023-10-18 18:39:40 +0000

    Update ternaryOperator

--- ternaryOperator.java
+++ ternaryOperator.java
@@ -3,9 +3,15 @@
 -	./ternaryOperator.java
 */
 /*
--	the opeator perform operation upon three operend that called ternary Operator.
+-	the opeator perform operation upon three operand that called ternary Operator.
 -	if expression is return true so execute first expression other else.
 -	it is very useful and short single if else code.
+
+[ control flow ]
+
+    1.the "condition" is an expression that evaluates to either ture or flase
+    2.if the condition is true, the value of "expressionIfTrue" is returned.
+    3.if the condititon is false, the value of "expressionIfFalse" is returned.
 
 [ syntax ]
 
